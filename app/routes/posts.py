34b1a96a18bba3528728e2029d1067ef3d2bf5b6
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-06-10 13:38:31 -0700

    added some CRUD endpoints for posts

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -17,3 +17,26 @@ def post(current_user):
     db.session.add(post)
     db.session.commit()
     return {'message': 'you just posted!'}
+
+
+@bp.route('/<id>', methods=['PUT'])
+@token_required
+def updateCaption(current_user, id):
+    data = request.json
+    db.session.query(Post).filter(Post.id == id).update({Post.caption: data['caption']})
+    db.session.commit()
+    post = Post.query.get(id)
+    post = {'id': post.id, 'image': post.image, 'caption': post.caption, 'user_id': post.user_id}
+    return {'data': post}
+
+@bp.route('/<id>', methods=['DELETE'])
+@token_required
+def deletePost(current_user, id):
+    post = Post.query.get(id)
+    if not post:
+        return {'message': 'you can\'t delete a post that doesn\'t exist!'}
+    if post.user_id == current_user.id:
+        db.session.query(Post).filter(Post.id == id).delete()
+        db.session.commit()
+        return {'message': 'you just deleted!'}
+    return {'message': 'you can\'t delete this!'}
