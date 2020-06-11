4efff21bd2a77493675f0c664937fd3618ba77a7
Seamus Le <seamust.le@gmail.com>
2020-06-11 13:43:25 -0700

    added route to handle follow req

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,12 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
 from .config import Configuration
-from .routes import follows, session, posts, users
+from .routes import session, posts, users
 from .models import db
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Configuration)
-app.register_blueprint(follows.bp)
 app.register_blueprint(session.bp)
 app.register_blueprint(posts.bp)
 app.register_blueprint(users.bp)
