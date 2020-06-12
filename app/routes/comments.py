506a44dba35db81b5bcd9f0d4c06e65c0e10bdba
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-06-12 09:53:15 -0700

    added delete comment route

--- app/routes/comments.py
+++ app/routes/comments.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request
+from sqlalchemy import and_
 from ..models import db, Comment
 from ..util import token_required
 
@@ -23,6 +24,7 @@ def getComments():
             returnDict[comment.post_id] = {
                 comment.id: {
                     'commenter': comment.commenter.username,
+                    'commenterId': comment.commenter.id,
                     'commenterAvi': comment.commenter.avatarUrl,
                     'body': comment.body,
                     'timestamp': comment.timestamp
@@ -31,6 +33,7 @@ def getComments():
         else:
             returnDict[comment.post_id][comment.id] = {
                 'commenter': comment.commenter.username,
+                'commenterId': comment.commenter.id,
                 'commenterAvi': comment.commenter.avatarUrl,
                 'body': comment.body,
                 'timestamp': comment.timestamp
@@ -43,7 +46,6 @@ def getComments():
 @token_required
 def addComment(current_user, postId):
     data = request.json
-    # print(data)
     newComment = Comment(
         post_id=postId,
         commenter=current_user,
@@ -54,33 +56,28 @@ def addComment(current_user, postId):
     comments = Comment.query.filter(Comment.post_id == postId).all()
     returnDict = dict((comment.id, {
             'commenter': comment.commenter.username,
+            'commenterId': comment.commenter.id,
+            'commenterAvi': comment.commenter.avatarUrl,
+            'body': comment.body,
+            'timestamp': comment.timestamp,
+        }) for comment in comments)
+    return returnDict
+
+
+@bp.route('/<int:commentId>', methods=['DELETE'])
+@token_required
+def deleteComment(current_user, commentId):
+    data = request.json
+    postId = data['postId']
+    db.session.query(Comment).filter(and_((Comment.id == commentId), (Comment.commenter == current_user))).delete()
+    db.session.commit()
+    comments = Comment.query.filter(Comment.post_id == postId).all()
+    returnDict = dict((comment.id, {
+            'commenter': comment.commenter.username,
+            'commenterId': comment.commenter.id,
             'commenterAvi': comment.commenter.avatarUrl,
             'body': comment.body,
             'timestamp': comment.timestamp,
         }) for comment in comments)
     # print(returnDict)
     return returnDict
-
-
-@bp.route('/<int:postId>', methods=['DELETE'])
-@token_required
-def addComment(current_user, postId):
-    data = request.json
-    Comment.query.filter(Comment.id).first()
-    # print(data)
-    # newComment = Comment(
-    #     post_id=postId,
-    #     commenter=current_user,
-    #     body=data['commentBody'],
-    # )
-    # db.session.add(newComment)
-    # db.session.commit()
-    # comments = Comment.query.filter(Comment.post_id == postId).all()
-    # returnDict = dict((comment.id, {
-    #     'commenter': comment.commenter.username,
-    #     'commenterAvi': comment.commenter.avatarUrl,
-    #     'body': comment.body,
-    #     'timestamp': comment.timestamp,
-    # }) for comment in comments)
-    # # print(returnDict)
-    # return returnDict
